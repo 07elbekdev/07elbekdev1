ffcbee2449bfdf17133595cc84463f646195dbcf
07elbekdev <masrapovelbek24@gmail.com>
2023-02-13 16:57:37 +0600

    Be careful everything is upto date.

--- src/main/java/org/example/configuration/Configuration.java
+++ src/main/java/org/example/configuration/Configuration.java
@@ -1,5 +1,6 @@
 package org.example.configuration;
 
+import org.example.models.Student;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Environment;
 
@@ -15,6 +16,7 @@ public class Configuration {
                 .setProperty(Environment.HBM2DDL_AUTO, "validate")//TODO : use this method differently, first you create it, than after creating the table, you should to change it , to 'validate'.
                 .setProperty(Environment.SHOW_SQL, "true")
                 .setProperty(Environment.DIALECT, "org.hibernate.dialect.PostgreSQL10Dialect")
+                .addAnnotatedClass(Student.class)
                 .buildSessionFactory();
         //here you should to use addAnnotated class and than it will work.
     }
---
+++ src/main/java/org/example/models/Student.java
@@ -0,0 +1,34 @@
+package org.example.models;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "student")
+@Data
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "student_Name")
+    private String name;
+    private int age;
+
+    public Student() {
+    }
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student { " +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                " } ";
+    }
+}
