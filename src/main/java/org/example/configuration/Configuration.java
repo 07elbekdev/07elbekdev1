fd562db89152265410be496fb33a2199906b0aad
kurmanbekdev <kurmanbekpazylo@gmail.com>
2023-02-13 14:09:41 +0300

    I commit

--- src/main/java/org/example/configuration/Configuration.java
+++ src/main/java/org/example/configuration/Configuration.java
@@ -10,7 +10,7 @@ public class Configuration {
         sessionFactory = new org.hibernate.cfg.Configuration()
                 .setProperty(Environment.DRIVER, "org.postgresql.Driver")
                 .setProperty(Environment.URL, "jdbc:postgresql://localhost:5432/postgres")
-                .setProperty(Environment.PASS, "iamnumber1")
+                .setProperty(Environment.PASS, "lenovo2000")
                 .setProperty(Environment.USER, "postgres")
                 .setProperty(Environment.HBM2DDL_AUTO, "validate")//TODO : use this method differently, first you create it, than after creating the table, you should to change it , to 'validate'.
                 .setProperty(Environment.SHOW_SQL, "true")
